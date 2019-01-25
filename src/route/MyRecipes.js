f0e28ff7cf7d8d2aa8e051b10b9b9bc84f523241
imallinson <imallinson1001@gmail.com>
2019-01-25 15:55:09 +0000

    my recipes

--- src/route/MyRecipes.js
+++ src/route/MyRecipes.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
-import RecipeSearchList from './RecipeSearchList.js'
+import RecipeSearchList from './RecipeSearchList.js';
+import axios from 'axios';
 
 class MyRecipes extends Component {
+    constructor() {
+        super();
+        this.state = {
+            recipeList: []
+        }
+    }
+
+    componentWillMount = () => {
+        var path = "http://localhost:8081/meal-planner/rest/recipe/user/" + this.props.username;
+        var actuallyThis = this;
+        axios({
+            method: 'get',
+            url: path,
+            responseType: 'json'
+        })
+        .then(function (response) {
+            actuallyThis.setState({
+                 recipeList: response.data
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    addRecipe = () => {
+        
+    }
+
     render() {
         return (
             <div className="container">
@@ -10,10 +40,10 @@ class MyRecipes extends Component {
                 </div>
                 <div className="row">
                     <div className="col12 center">
-                        <button className="btn grey darken-2" type="submit">Add a Recipe</button>
+                        <button className="btn grey darken-2" type="button" onClick={this.addRecipe} >Add a Recipe</button>
                     </div>
-                </div>                
-                <RecipeSearchList username={this.props.username} editable={true} />
+                </div>             
+                <RecipeSearchList recipeList={this.state.recipeList} editable={true} />
             </div>
         );
     }
--- src/route/RecipePage.js
+++ src/route/RecipePage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import materialize from 'materialize';
 
-class RecipePage extends Component {
+class RecipePage extends React.Component {
     render() {
         return (
-            <div>
-                Recipe Page
+            <div className='popup'>
+                <div className='popup_inner'>
+                    <h1>recipe</h1>
+                    <button onClick={this.props.closePopup}><i class="large material-icons">close</i></button>
+                </div>
             </div>
         );
     }
